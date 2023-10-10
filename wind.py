968182db5b04129e1ee949d69d61f7d095b87f11
Mark Ortega <markortega@Marks-MBP.attlocal.net>
2023-10-09 21:42:22 -0700

    field gravitates towards ball

--- wind.py
+++ wind.py
@@ -1,30 +1,39 @@
 from manim import *
 import numpy as np
 
-class VectorFieldScene(ThreeDScene):
+class GravityField(ThreeDScene):
     def construct(self):
         self.set_camera_orientation(phi=60 * DEGREES, theta=45 * DEGREES)
-        
-        # Create a vector field
-        vector_field = self.get_vector_field()
+
+        # Create a vector field with all vectors pointing downwards initially
+        vector_field = self.get_vector_field(DOWN * 2)
         self.add(vector_field)
 
-        self.wait(5)
-    
-    def get_vector_field(self):
-        # Define vector field function
-        def vector_field_func(point):
-            x, y, z = point
-            return np.array([y, -x, z])  # Example function; modify as needed
+        # Create a sphere that will move to simulate a planet
+        sphere = Sphere(radius=0.2, color=RED)
+        sphere.shift(DOWN * 2)
+        self.add(sphere)
 
-        # Generate vectors for the field
+        # Animate the sphere moving upward while updating the vector field
+        for _ in range(40):
+            sphere.shift(UP * 0.1)
+            new_field = self.get_vector_field(sphere.get_center())
+            self.play(
+                Transform(vector_field, new_field),
+                sphere.animate.shift(UP * 0.1),
+                run_time=0.1
+            )
+        
+        self.wait(1)
+
+    def get_vector_field(self, sphere_center):
+        # Generate vectors that point towards the sphere
         vectors = VGroup()
         for x in np.arange(-2, 2.1, 0.5):
             for y in np.arange(-2, 2.1, 0.5):
                 for z in np.arange(-2, 2.1, 0.5):
                     point = np.array([x, y, z])
-                    vector = vector_field_func(point)
-                    vectors.add(Arrow(point, point + vector * 0.3, buff=0, color=YELLOW))
+                    direction = normalize(sphere_center - point) * 0.3
+                    vectors.add(Arrow(point, point + direction, buff=0, color=YELLOW))
 
         return vectors
-
