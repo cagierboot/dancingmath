0ceb858fd48f4b1004056aa56bf07ac592159bdb
Mark Ortega <markortega@wifi-10-40-34-47.wifi.berkeley.edu>
2023-10-10 10:53:22 -0700

    cube rotates smoothly

--- complex.py
+++ complex.py
@@ -1,39 +1,21 @@
 from manim import *
 
-
-class CreateGraph(ThreeDScene):
+class Create3DCube(ThreeDScene):
     def construct(self):
-        # Create a Line
-        line = Line(start=LEFT, end=RIGHT)
-        self.play(Create(line))
-        self.wait(1)
+        # Create a cube
+        cube = Cube()
 
-        # Turn the Line into a Square
-        square = Square()
-        self.play(Transform(line, square))
-        self.wait(1)
+        # Set the initial camera orientation to isometric view
+        self.set_camera_orientation(phi=PI / 4, theta=PI / 4)
 
-        # Turn the Square into a 3D square (Cube) with see-through faces
-        cube = Cube(fill_opacity=0)
-        self.play(Transform(square, cube))
+        # Add the cube
+        self.add(cube)
 
-        # Create white edges for the cube
-        edges = [
-            Line(start=cube.get_corner(start), end=cube.get_corner(end), color=WHITE)
-            for start, end in [
-                (DLF, DLF + RIGHT), (DLF + RIGHT, DLF + RIGHT + UP), 
-                (DLF + RIGHT + UP, DLF + UP), (DLF + UP, DLF),
-                (DLF, DLF + OUT), (DLF + RIGHT, DLF + RIGHT + OUT), 
-                (DLF + RIGHT + UP, DLF + RIGHT + UP + OUT), 
-                (DLF + UP, DLF + UP + OUT),
-                (DLF + OUT, DLF + OUT + RIGHT), (DLF + OUT + RIGHT, DLF + OUT + RIGHT + UP), 
-                (DLF + OUT + RIGHT + UP, DLF + OUT + UP), (DLF + OUT + UP, DLF + OUT)
-            ]
-        ]
+        # Start the camera rotation
+        self.begin_ambient_camera_rotation(rate=0.2)
 
-        self.play(*[Create(edge) for edge in edges])
-        self.wait(1)
+        # Show the cube for 5 seconds
+        self.wait(5)
 
-        # Slowly move Camera to show 3D structure of the cube isometrically
-        self.move_camera(phi=45*DEGREES, theta=45*DEGREES, run_time=3)
-        self.wait(2)
+        # Stop the camera rotation
+        self.stop_ambient_camera_rotation()
