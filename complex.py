d5092f7da664ef9fb44ccbd4ab61678217969bd7
Mark Ortega <markortega@wifi-10-40-34-47.wifi.berkeley.edu>
2023-10-10 13:54:48 -0700

    fire animation

--- complex.py
+++ complex.py
@@ -14,8 +14,8 @@ class Create3DCube(ThreeDScene):
         # Set the stroke color and width to highlight the edges if needed
         cube.set_stroke(color=WHITE, width=2)
 
-        # Set the initial camera orientation to isometric view
-        self.set_camera_orientation(phi=PI / 4, theta=PI / 4)
+        # Set the initial camera orientation to straight forward view
+        self.set_camera_orientation(phi=0, theta=0)  # Adjust these angles as needed
 
         # Add the line
         self.play(Create(line))
@@ -31,6 +31,9 @@ class Create3DCube(ThreeDScene):
 
         # Transform the square into a cube with increased run_time
         self.play(ReplacementTransform(square, cube), run_time=3)  # Increased run_time to 3 seconds
+
+        # Move the camera to the isometric view
+        self.move_camera(phi=PI/4, theta=PI/4, run_time=2)  # Adjusting the run_time as needed
 
         # Start the camera rotation
         self.begin_ambient_camera_rotation(rate=0.2)
