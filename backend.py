9cfd33c438974a3b55209e0ff626312f39835642
Mark Ortega <markortega@Marks-MBP.attlocal.net>
2023-10-11 00:01:38 -0700

    trying to display a video

--- backend.py
+++ backend.py
@@ -1,9 +1,15 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, send_file, jsonify
+
+app = Flask(__name__)  # Make sure this line is before any @app.route decorators
 
 @app.route('/')
 def hello_world():
     return "Hello, World"
+
+@app.route('/video')
+def video():
+    return send_file('HomescreenVideo.mp4', mimetype='video/mp4')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
