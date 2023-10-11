990cc3329a497a214fb2019de92fd80f1b4afb2e
Mark Ortega <markortega@wifi-10-40-34-47.wifi.berkeley.edu>
2023-10-11 13:27:10 -0700

    frontend is connected

--- backend.py
+++ backend.py
@@ -1,19 +1,12 @@
-from flask import Flask, send_file, jsonify
+from flask import Flask
 from flask_cors import CORS
   
-app = Flask(__name__)  # Make sure this line is before any @app.route decorators
+app = Flask(__name__)  
+CORS(app)  # This allows CORS for all routes
 
 @app.route('/')
 def hello_world():
     return "Hello, Mark"
-
-@app.route('/video')
-def video():
-    print("Video endpoint hit")
-    try:
-        return send_file('HomescreenVideo.mp4', mimetype='video/mp4')
-    except Exception as e:
-        print("Error sending file: ", e)
 
 if __name__ == '__main__':
     app.run(debug=True)
