83783ed6acd98e3429f34ada7fe6fa9108adf366
Mark Ortega <markortega@Marks-MBP.attlocal.net>
2023-10-12 20:30:53 -0700

    perfectly renders cube at beat drop

--- dimension1.py
+++ dimension1.py
@@ -1,6 +1,14 @@
 import librosa
-from manim import *
 import numpy as np
+from manim import *
+
+# Function to generate the vertices of a hexagon
+def hexagon_points(radius=1):
+    """Return the vertices of a regular hexagon with the given radius."""
+    return [
+        np.array([np.cos(np.pi / 3 * i) * radius, np.sin(np.pi / 3 * i) * radius, 0])
+        for i in range(6)
+    ]
 
 # Load the song and get the beat timings
 y, sr = librosa.load("expanse.mp3")
@@ -18,7 +26,7 @@ loudness_beat = loudness[:, time_index].mean(axis=0)
 # Normalize the loudness values to a scale from 0 to 1 for scaling shapes
 loudness = (loudness_beat - np.min(loudness_beat)) / (np.max(loudness_beat) - np.min(loudness_beat))
 
-class Create3DCubeWithoutEquations(ThreeDScene):
+class CreateCubeAtBeatDrop(ThreeDScene):
     def construct(self):
         total_beats = len(beat_times)
         if total_beats == 0:
@@ -28,11 +36,10 @@ class Create3DCubeWithoutEquations(ThreeDScene):
         initial_shape_factories = [
             lambda: Line(start=[-1,0,0], end=[1,0,0], color=WHITE),
             lambda: Circle(color=WHITE),
-            lambda: Square(color=WHITE)
+            lambda: Square(color=WHITE),
+            lambda: Triangle(color=WHITE),
+            lambda: Polygon(*hexagon_points(radius=1), color=WHITE)  # Adjust the radius as needed
         ]
-
-        # All shape factories including the cube after the beat drops
-        all_shape_factories = initial_shape_factories + [lambda: Cube(fill_opacity=0).set_stroke(color=WHITE, width=2)]
 
         # Use the initial shapes before the beat drops
         shape_factories = initial_shape_factories
@@ -46,11 +53,12 @@ class Create3DCubeWithoutEquations(ThreeDScene):
 
             if not beat_dropped and loudness[i] > 0.8:  # Adjust the threshold as needed to detect the beat drop
                 beat_dropped = True
-                # Switch to including the cube in the shape cycle after the beat drops
-                shape_factories = all_shape_factories
+                # Make the next shape a cube specifically at the beat drop
+                next_shape = Cube(fill_opacity=0).set_stroke(color=WHITE, width=2)
+            else:
+                next_shape_index = i % len(shape_factories)
+                next_shape = shape_factories[next_shape_index]()
 
-            next_shape_index = i % len(shape_factories)
-            next_shape = shape_factories[next_shape_index]()
             scaling_factor = 0.5 + loudness[i] * 3
             next_shape.scale(scaling_factor)
 
@@ -62,5 +70,5 @@ class Create3DCubeWithoutEquations(ThreeDScene):
             shape = next_shape
             elapsed_time += duration
 
-scene = Create3DCubeWithoutEquations()
+scene = CreateCubeAtBeatDrop()
 scene.render()
