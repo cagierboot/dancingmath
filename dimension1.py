1bc46230573ae64aa855c286490f27996b8ad7fa
Mark Ortega <markortega@wifi-10-40-35-17.wifi.berkeley.edu>
2023-10-17 12:54:20 -0700

    equations render

--- dimension1.py
+++ dimension1.py
@@ -32,56 +32,58 @@ class CreateShapesToBeat(ThreeDScene):
         if total_beats == 0:
             raise Exception("No beats found in the audio file.")
 
-        # Create a white rectangle to cover the entire scene
-        background_rect = Rectangle(height=config.frame_height, width=config.frame_width, fill_color=WHITE, fill_opacity=0)
-        self.add(background_rect)
-
-        # All available shape factories
+        # All available shape factories with their respective equations
         shape_factories = [
-            lambda: Cube(fill_opacity=0).set_stroke(color=WHITE, width=2),
-            lambda: Circle(color=WHITE),
-            lambda: Sphere(fill_opacity=0).set_stroke(color=WHITE, width=2),
-            lambda: Line(start=[-1, 0, 0], end=[1, 0, 0], color=WHITE),
-            lambda: Triangle(color=WHITE),
-            lambda: Square(color=WHITE),
-            lambda: Polygon(*regular_polygon_points(6, radius=1), color=WHITE),  # Hexagon
-            lambda: Ellipse(width=2, height=1, color=WHITE),
-            lambda: Prism(dimensions=[2, 2, 2], fill_opacity=0).set_stroke(color=WHITE, width=2)
-        ]
+    (lambda: Cube(fill_opacity=0).set_stroke(color=WHITE, width=2), r"$x^2+y^2+z^2=r^2$"),
+    (lambda: Circle(color=WHITE), r"$x^2+y^2=r^2$"),
+    (lambda: Sphere(fill_opacity=0).set_stroke(color=WHITE, width=2), r"$x^2+y^2+z^2=r^2$"),
+    (lambda: Line(start=[-1, 0, 0], end=[1, 0, 0], color=WHITE), r"$y=mx+c$"),
+    (lambda: Triangle(color=WHITE), r"$A=\frac{1}{2}bh$"),
+    (lambda: Square(color=WHITE), r"$A=s^2$"),
+    (lambda: Polygon(*regular_polygon_points(6, radius=1), color=WHITE), r"$A=\frac{3\sqrt{3}}{2}s^2$"),
+    (lambda: Ellipse(width=2, height=1, color=WHITE), r"$\frac{x^2}{a^2}+\frac{y^2}{b^2}=1$"),
+    (lambda: Prism(dimensions=[2, 2, 2], fill_opacity=0).set_stroke(color=WHITE, width=2), r"$V=Bh$")
+]
 
         # Force the first 4 shapes and then begin the random generations
         elapsed_time = 0
         shape = None
+        equation = None
 
         for i in range(total_beats):
-            if i < 4:
-                next_shape = shape_factories[i]()
+            if i < len(shape_factories):
+                next_shape, eq_text = shape_factories[i]
+                next_shape = next_shape()
             else:
-                random.shuffle(shape_factories[4:])
-                next_shape_index = i % len(shape_factories[4:]) + 4
-                next_shape = shape_factories[next_shape_index]()
+                random.shuffle(shape_factories)  # Randomize the order of shapes on each iteration
+                next_shape, eq_text = random.choice(shape_factories)
+                next_shape = next_shape()
+
+            # Create equation text
+            next_equation = Tex(eq_text, color=WHITE).scale(0.5)
+            next_equation.next_to(next_shape, DOWN)
 
             # Scaling the shape according to the loudness
-            scaling_factor = 0.5 + loudness[i] * 3
+            scaling_factor = 0.5 + loudness[i] * 3  # You can adjust the base size and multiplier as needed
             next_shape.scale(scaling_factor)
+            next_equation.scale(scaling_factor)
 
-            if shape is not None:
-                shape.clear_updaters()
-
-            if shape:
+            if shape and equation:
                 self.play(
                     ReplacementTransform(shape, next_shape),
+                    ReplacementTransform(equation, next_equation),
                     run_time=beat_times[i] - elapsed_time if i != 0 else 0.1
                 )
-                
-                self.play(background_rect.animate.set_opacity(1), run_time=0.1)
-                self.play(background_rect.animate.set_opacity(0), run_time=0.1)
             else:
-                self.play(FadeIn(next_shape), run_time=beat_times[i] - elapsed_time if i != 0 else 0.1)
-            
+               self.play(Create(VGroup(next_shape, next_equation)),
+                          run_time=beat_times[i] - elapsed_time if i != 0 else 0.1)
+
             next_shape.add_updater(lambda m, dt: m.rotate(0.05, axis=UP))
             shape = next_shape
+            equation = next_equation
             elapsed_time = beat_times[i] if i != 0 else 0.1
+
+        shape.clear_updaters()
 
 scene = CreateShapesToBeat()
 scene.render()
