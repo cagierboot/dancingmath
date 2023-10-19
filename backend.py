0ea80e6caa9f91f37dfe8c327ad94fc4a61aedb3
Mark Ortega <markortega@wifi-10-40-32-214.wifi.berkeley.edu>
2023-10-19 12:40:00 -0700

    indicates when audio file is uplaoded

--- backend.py
+++ backend.py
@@ -1,12 +1,99 @@
-from flask import Flask
+from flask import Flask, send_file, jsonify
 from flask_cors import CORS
-  
-app = Flask(__name__)  
+import librosa
+import numpy as np
+import random
+from manim import *
+
+app = Flask(__name__)
 CORS(app)  # This allows CORS for all routes
+
+def pentagon_points(radius=1):
+    return [
+        np.array([np.cos(np.pi / 2.5 * i) * radius, np.sin(np.pi / 2.5 * i) * radius, 0])
+        for i in range(5)
+    ]
+
+class CreateCubeAtBeatDrop(ThreeDScene):
+    def construct(self):
+        y, sr = librosa.load("expanse.mp3")
+        tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+        beat_times = librosa.frames_to_time(beat_frames, sr=sr)
+
+        S = np.abs(librosa.stft(y))
+        loudness = librosa.amplitude_to_db(S, ref=np.max)
+        time_index = (beat_times * sr / 512).astype(int)
+        loudness_beat = loudness[:, time_index].mean(axis=0)
+        loudness = (loudness_beat - np.min(loudness_beat)) / (np.max(loudness_beat) - np.min(loudness_beat))
+
+        total_beats = len(beat_times)
+        if total_beats == 0:
+            raise Exception("No beats found in the audio file.")
+
+        initial_shape_factories = [
+            lambda: Line(start=[-1, 0, 0], end=[1, 0, 0], color=WHITE),
+            lambda: Triangle(color=WHITE),
+            lambda: Polygon(*pentagon_points(radius=1), color=WHITE),
+            lambda: Circle(color=WHITE),
+            lambda: Square(color=WHITE),
+        ]
+
+        shape_factories = initial_shape_factories.copy()
+
+        elapsed_time = 0
+        shape = shape_factories[0]()
+
+        beat_dropped = False
+
+        for i in range(total_beats):
+            duration = beat_times[i] - elapsed_time if i != 0 else 0.1
+
+            if not beat_dropped and loudness[i] > 0.8:
+                beat_dropped = True
+                shape_factories.extend([
+                    lambda: Cube(fill_opacity=0).set_stroke(color=WHITE, width=2),
+                    lambda: Sphere(fill_opacity=0).set_stroke(color=WHITE, width=2),
+                    lambda: Cone(fill_opacity=0).set_stroke(color=WHITE, width=2).scale(np.array([1, 3, 1])),
+                    lambda: Prism(dimensions=[2, 2, 2], fill_opacity=0).set_stroke(color=WHITE, width=2)
+                ])
+
+                random.shuffle(shape_factories)
+
+            next_shape = shape_factories[i % len(shape_factories)]()
+            scaling_factor = 0.5 + loudness[i] * 3
+            next_shape.scale(scaling_factor)
+
+            self.play(
+                ReplacementTransform(shape, next_shape),
+                run_time=duration
+            )
+
+            next_shape.add_updater(lambda m, dt: m.rotate(0.05, axis=UP))
+            shape = next_shape
+            elapsed_time += duration
+
+        shape.clear_updaters()
 
 @app.route('/')
 def hello_world():
     return "Hello, Mark"
+
+@app.route('/render_video', methods=['POST'])
+def render_video():
+    try:
+        audio_file = request.files['audio']
+        audio_filepath = "/tmp/" + audio_file.filename  # Adjust the path as needed
+        audio_file.save(audio_filepath)
+        
+        scene = CreateCubeAtBeatDrop(audio_filepath=audio_filepath)
+        video_path = "/tmp/video.mp4"  # Adjust the path as needed
+        scene.render(file_path=video_path, save_last_frame=True)
+
+        os.remove(audio_filepath)  # Optional: remove the saved audio file after processing
+
+        return send_file(video_path, as_attachment=True)
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- dimension1.py
+++ dimension1.py
@@ -3,15 +3,16 @@ import numpy as np
 import random
 from manim import *
 
-# Function to generate the vertices of a regular polygon
-def regular_polygon_points(sides, radius=1):
+# Function to generate the vertices of a pentagon
+def pentagon_points(radius=1):
+    """Return the vertices of a regular pentagon with the given radius."""
     return [
-        np.array([np.cos(2 * np.pi / sides * i) * radius, np.sin(2 * np.pi / sides * i) * radius, 0])
-        for i in range(sides)
+        np.array([np.cos(np.pi / 2.5 * i) * radius, np.sin(np.pi / 2.5 * i) * radius, 0])
+        for i in range(5)
     ]
 
 # Load the song and get the beat timings
-y, sr = librosa.load("interstellar.mp3")
+y, sr = librosa.load("expanse.mp3")
 tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
 beat_times = librosa.frames_to_time(beat_frames, sr=sr)
 
@@ -19,113 +20,77 @@ beat_times = librosa.frames_to_time(beat_frames, sr=sr)
 S = np.abs(librosa.stft(y))
 loudness = librosa.amplitude_to_db(S, ref=np.max)
 
-# Get the loudness at each beat
+# Get the loudness at each beat by converting beat times to indices on the time axis
 time_index = (beat_times * sr / 512).astype(int)
 loudness_beat = loudness[:, time_index].mean(axis=0)
 
-# Normalize the loudness values
+# Normalize the loudness values to a scale from 0 to 1 for scaling shapes
 loudness = (loudness_beat - np.min(loudness_beat)) / (np.max(loudness_beat) - np.min(loudness_beat))
 
-class CreateShapesToBeat(ThreeDScene):
+class CreateCubeAtBeatDrop(ThreeDScene):
     def construct(self):
         total_beats = len(beat_times)
         if total_beats == 0:
             raise Exception("No beats found in the audio file.")
 
+        # Initial shape factories before the beat drops
+        initial_shape_factories = [
+            lambda: Line(start=[-1, 0, 0], end=[1, 0, 0], color=WHITE),
+            lambda: Triangle(color=WHITE),
+            lambda: Polygon(*pentagon_points(radius=1), color=WHITE),
+            lambda: Circle(color=WHITE),
+            lambda: Square(color=WHITE),
+        ]
 
-class CreateShapesToBeat(ThreeDScene):
-    def construct(self):
-        total_beats = len(beat_times)
-        if total_beats == 0:
-            raise Exception("No beats found in the audio file.")
+        # Use the initial shapes before the beat drops
+        shape_factories = initial_shape_factories.copy()
 
-        # All available shape factories with their respective equations
-        shape_factories = [
-    (lambda: Cube(fill_opacity=0).set_stroke(color=WHITE, width=2), 
-    r"$-\frac{s}{2} \leq x \leq \frac{s}{2}$, $-\frac{s}{2} \leq y \leq \frac{s}{2}$, $-\frac{s}{2} \leq z \leq \frac{s}{2}$"),
-    
-    (lambda: Circle(color=WHITE), r"$(x - h)^2 + (y - k)^2 = r^2$"),  # Added a comma at the end here
-
-    (lambda: Sphere(fill_opacity=0).set_stroke(color=WHITE, width=2), r"$x^2+y^2+z^2=r^2$"),
-    (lambda: Line(start=[-1, 0, 0], end=[1, 0, 0], color=WHITE), r"$y=mx+c$"),
-    (lambda: Triangle(color=WHITE), r"$A=\frac{1}{2}bh$"),
-    (lambda: Square(color=WHITE), r"$A=s^2$"),
-    (lambda: Polygon(*regular_polygon_points(6, radius=1), color=WHITE), r"$A=\frac{3\sqrt{3}}{2}s^2$"),
-    (lambda: Ellipse(width=2, height=1, color=WHITE), r"$\frac{x^2}{a^2}+\frac{y^2}{b^2}=1$"),
-    (lambda: Prism(dimensions=[2, 2, 2], fill_opacity=0).set_stroke(color=WHITE, width=2), r"$V=Bh$"),
-    (lambda: Polygon(*regular_polygon_points(8, radius=1), color=WHITE), r"$A=2s^2(1+\sqrt{2})$")
-]
-
-
-        # Force the first 4 shapes and then begin the random generations
         elapsed_time = 0
-        shape = None
-        equation = None
+        shape = shape_factories[0]()
+
+        beat_dropped = False
 
         for i in range(total_beats):
-            if i < len(shape_factories):
-                next_shape, eq_text = shape_factories[i]
-                next_shape = next_shape()
-            else:
-                random.shuffle(shape_factories)  # Randomize the order of shapes on each iteration
-                next_shape, eq_text = random.choice(shape_factories)
-                next_shape = next_shape()
+            duration = beat_times[i] - elapsed_time if i != 0 else 0.1
 
-            # Create equation text
-            next_equation = Tex(eq_text, color=WHITE).scale(0.5)
-            next_equation.next_to(next_shape, DOWN)
+            if not beat_dropped and loudness[i] > 0.8:
+                beat_dropped = True
+                next_shape = Cube(fill_opacity=0).set_stroke(color=WHITE, width=2)
+                shape_factories.append(lambda: Cube(fill_opacity=0).set_stroke(color=WHITE, width=2))
 
-            # Scaling the shape according to the loudness
-            scaling_factor = 0.5 + loudness[i] * 2.5  # You can adjust the base size and multiplier as needed
-            next_shape.scale(scaling_factor)
-            next_equation.scale(scaling_factor)
+                shape_factories.append(lambda: Sphere(fill_opacity=0).set_stroke(color=WHITE, width=2))
 
-           # Update the position of the equation based on the boundaries of the shape
-            shape_bottom = next_shape.get_critical_point(DOWN)[1]
-            equation_top = next_equation.get_critical_point(UP)[1]
-            next_equation.next_to(next_shape, DOWN, buff=(equation_top - shape_bottom) * .2)
-            
-            # Calculate the combined bounding box of the shape and equation
-            combined = VGroup(next_shape, next_equation)
-            combined_width = combined.get_width()
-            combined_height = combined.get_height()
-            
-            scale_factor = min(config.frame_width / combined_width, config.frame_height / combined_height * .7)
-            
-            # If the bounding box is larger than the viewport, scale down both the shape and equation
-            if scale_factor < 1:
-                next_shape.scale(scale_factor)
-                next_equation.scale(scale_factor)
-            
-            if shape and equation:
-                self.play(
-                    ReplacementTransform(shape, next_shape),
-                    ReplacementTransform(equation, next_equation),
-                    run_time=beat_times[i] - elapsed_time if i != 0 else 0.1
+                shape_factories.append(
+                    lambda: Cone(fill_opacity=0).set_stroke(color=WHITE, width=2).scale(np.array([1, 3, 1]))
+
                 )
+                shape_factories.append(lambda: Prism(dimensions=[2, 2, 2], fill_opacity=0).set_stroke(color=WHITE, width=2))
+
+                # Shuffle the shapes after the beat drops
+                random.shuffle(shape_factories)
+
             else:
-               self.play(Create(VGroup(next_shape, next_equation)),
-                          run_time=beat_times[i] - elapsed_time if i != 0 else 0.1)
+                next_shape_index = i % len(shape_factories)
+                next_shape = shape_factories[next_shape_index]()
 
-            next_shape.add_updater(lambda m, dt: m.rotate(0.2, axis=UP))
+            scaling_factor = 0.5 + loudness[i] * 3
+            next_shape.scale(scaling_factor)
+
+            self.play(
+                ReplacementTransform(shape, next_shape),
+                run_time=duration
+            )
+
+            # Adding rotation with add_updater
+            next_shape.add_updater(lambda m, dt: m.rotate(0.05, axis=UP))
+
+
+
             shape = next_shape
-            equation = next_equation
-            elapsed_time = beat_times[i] if i != 0 else 0.1
-
+            elapsed_time += duration
+            
+        # To remove updater when animation is done to prevent continuation of rotation after scene end
         shape.clear_updaters()
 
-class PortraitConfig:
-    def __init__(self):
-        config.pixel_height = 3000
-        config.pixel_width = 2050
-        config.frame_height = 8.0
-        config.frame_width = config.frame_height * (config.pixel_width / config.pixel_height)
-
-# Create an instance of the configuration class
-PortraitConfig()
-
-# Create an instance of the scene class with the custom configuration
-scene = CreateShapesToBeat()
-
-# Render the scene
+scene = CreateCubeAtBeatDrop()
 scene.render()
